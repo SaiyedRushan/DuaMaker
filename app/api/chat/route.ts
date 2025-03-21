06e0e114ffc9250b719d311f5d00986396b669ea
Rushanshah Saiyed <rushan52@gmail.com>
2025-03-21 00:42:37 -0400

    replace with gemini model and make edge function to increase timeout

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,18 +1,19 @@
 import { OpenAI } from 'openai'
 import { NextRequest, NextResponse } from 'next/server'
+export const config = { runtime: 'edge' } // Increases timeout to 30s
 
 const openai = new OpenAI({
-  baseURL: 'https://api.deepseek.com',
-  apiKey: process.env.DEEPSEEK_API_KEY,
+  baseURL: 'https://generativelanguage.googleapis.com/v1beta/openai/',
+  apiKey: process.env.GEMINI_API_KEY,
 })
 
 export async function POST(req: NextRequest) {
   try {
     const instructions =
-      'Please enhance the dua that the user enters. Use Allahs name and attributes that are relevant to the dua to make the dua more powerful. Only respond with the enhanced dua, no other text.'
+      'Please enhance the dua that the user enters. Use all the names of Allah and attributes that are relevant to the dua to make the dua more powerful. Provide english meaning in brackets for the names. Only respond with the enhanced dua, no other text.'
     const { message } = await req.json()
     const response = await openai.chat.completions.create({
-      model: 'deepseek-chat',
+      model: 'gemini-2.0-flash',
       messages: [
         { role: 'system', content: instructions },
         { role: 'user', content: message },
